a3cef1740376d5e1ae0d0a1618772107a42a2519
uraf <piotrekmis@gmail.com>
2018-02-10 12:43:56 +0100

    Splitting into proper subprojects

---
+++ profix_derive/src/lib.rs
@@ -0,0 +1,305 @@
+#![recursion_limit="32768"]
+
+extern crate proc_macro;
+extern crate syn;
+#[macro_use]
+extern crate quote;
+
+use proc_macro::TokenStream;
+
+#[proc_macro_derive(FixSerialize, attributes(msg_type, id))]
+pub fn fix_serialize(input: TokenStream) -> TokenStream {
+    let s = input.to_string();
+    let ast = syn::parse_derive_input(&s).unwrap();
+    let gen = impl_fix_serialize(ast);
+    gen.parse().unwrap()
+}
+
+#[proc_macro_derive(FixDeserialize, attributes(msg_type, id))]
+pub fn fix_deserialize(input: TokenStream) -> TokenStream {
+    let s = input.to_string();
+    let ast = syn::parse_derive_input(&s).unwrap();
+    let gen = impl_fix_deserialize(ast);
+    match gen.parse() {
+        Ok(ts) => ts,
+        Err(e) => panic!("{:?}: {:?}", e, gen),
+    }
+}
+
+#[proc_macro_derive(FixParse, attributes(fix_value))]
+pub fn fix_parse(input: TokenStream) -> TokenStream {
+    let s = input.to_string();
+    let ast = syn::parse_derive_input(&s).unwrap();
+    let gen = impl_fix_parse(ast);
+    match gen.parse() {
+        Ok(ts) => ts,
+        Err(e) => panic!("{:?}: {:?}", e, gen),
+    }
+}
+
+
+
+fn impl_fix_serialize(ast: syn::DeriveInput) -> quote::Tokens {
+    let name = &ast.ident;
+    let msg_type = find_attr("msg_type", &ast.attrs);
+
+    if let syn::Body::Struct(syn::VariantData::Struct(fields)) = ast.body {
+        let fields = find_fix_fields(&fields);
+
+        let ids = fields.iter().map(|f| f.id.to_string());
+        let idents = fields.iter().map(|f| f.ident.clone());
+        let dummy_const = syn::Ident::new(format!("_IMPL_FIX_SERIALIZE_FOR_{}", name));
+
+        quote! {
+            #[allow(non_upper_case_globals)]
+            const #dummy_const: () = {
+                extern crate fix;
+                impl fix::detail::FixSerializable for #name {
+                    fn serialize_body_to_fix(&self) -> String {
+                        format!(concat!("35=", #msg_type, "\x01", concat!(#(#ids, "={}\x01"),* )), #(self.#idents),*)
+                    }
+                }
+            };
+        }
+    } else {
+        panic!("#[derive(FixSerialize)] is only defined for structs")
+    }
+}
+
+fn impl_fix_deserialize(ast: syn::DeriveInput) -> quote::Tokens {
+    match ast.body {
+        syn::Body::Struct(syn::VariantData::Struct(fields)) => {
+            impl_fix_deserialize_struct(ast.ident, ast.attrs, fields)
+        },
+        syn::Body::Enum(variants) => {
+            impl_fix_deserialize_enum(ast.ident, variants)
+        },
+        _ => {
+            panic!("#[derive(FixDeserialize)] is only defined for structs and enums")
+        },
+    }
+}
+
+fn impl_fix_parse(ast: syn::DeriveInput) -> quote::Tokens {
+    match ast.body {
+        syn::Body::Enum(variants) => {
+            impl_fix_parse_enum(ast.ident, variants)
+        },
+        _ => {
+            panic!("#[derive(FixParse)] is only defined for enums")
+        },
+    }
+}
+
+fn impl_fix_parse_enum(name: syn::Ident, variants: Vec<syn::Variant>) -> quote::Tokens {
+    let pairs: Vec<_> = variants.iter().map(|variant| {
+        match variant.data {
+            syn::VariantData::Unit => {
+                let attr = find_attr("fix_value", &variant.attrs);
+                (attr, variant.ident.clone())
+            },
+            _ => panic!("#[derive(FixParse)] Only unit variants are supported for enums."),
+        }
+    }).collect();
+
+    let names: Vec<_> = pairs.iter().map(|_| name.clone()).collect();
+    let values: Vec<_> = pairs.iter().map(|p| p.0.as_bytes()).collect();
+    let idents: Vec<_> = pairs.iter().map(|p| p.1.clone()).collect();
+    let dummy_const = syn::Ident::new(format!("_IMPL_FIX_PARSE_FOR_{}", name));
+
+    let tokens = quote! {
+        #[allow(non_upper_case_globals)]
+        const #dummy_const: () = {
+            extern crate fix;
+            impl fix::FixParse for #name {
+                fn parse(value: &[u8]) -> Result<Self, fix::ParseError> {
+                    #(
+                        if value == #values {
+                            return Ok(#names::#idents);
+                        }
+                    )*
+
+                    Err(concat!(stringify!(#name), " could not parse enum value"))
+                }
+            }
+        };
+    };
+    tokens
+}
+
+fn impl_fix_deserialize_struct(name: syn::Ident, attrs: Vec<syn::Attribute>, fields: Vec<syn::Field>) -> quote::Tokens {
+    const CHECKSUM_ID: u64 = 10;
+
+    let msg_type = find_attr("msg_type", &attrs);
+    let msg_type_bytes = msg_type.as_bytes();
+
+    let fields = find_fix_fields(&fields);
+
+    let ids: Vec<_> = fields.iter().map(|f| f.id).collect();
+    let outs: Vec<_> = fields.iter().map(|f| &f.ident).collect();
+    let names: Vec<_> = fields.iter().map(|_| name.to_string()).collect();
+
+    let (outs1, outs2, outs3, outs4, outs5, outs6, outs7) = (outs.clone(), outs.clone(), outs.clone(), outs.clone(), outs.clone(), outs.clone(), outs.clone());
+    let (names1, names2) = (names.clone(), names.clone());
+
+    let dummy_const = syn::Ident::new(format!("_IMPL_FIX_DESERIALIZE_FOR_{}", name));
+    let tokens = quote! {
+        #[allow(non_upper_case_globals)]
+        const #dummy_const: () = {
+            extern crate fix;
+            use std::num::Wrapping;
+
+            impl fix::detail::FixMessageType for #name {
+                const MSG_TYPE: &'static [u8] = &#msg_type_bytes;
+            }
+
+            impl fix::detail::FixDeserializable for #name {
+                fn deserialize_from_fix(msg: fix::detail::FixMessage) -> Result<Self, fix::ParseError> {
+                    #(
+                        let mut #outs1 = Err(concat!(#names1, " missing ", stringify!(#outs2)));
+                    )*
+
+                    let mut input = msg.body;
+                    let mut checksum = msg.header_checksum;
+                    loop {
+                        let field = fix::detail::parse_fix_field(input)?;
+
+                        match field.id {
+                            #(
+                                #ids => {
+                                    if #outs3.is_err() {
+                                        #outs4 = Ok(fix::FixParse::parse(field.value)?);
+                                    } else {
+                                        return Err(concat!(#names2, " found multiple ", stringify!(#outs5)));
+                                    }
+                                },
+                            )*
+                            #CHECKSUM_ID => {
+                            // TODO: Fix this
+                            /*
+                                if input.len() != field.length {
+                                    return Err(concat!(stringify!(#name), " detected input after checksum"));
+                                }
+                            */
+                                let parsed_checksum: u8 = fix::FixParse::parse(field.value)?;
+                                if Wrapping(parsed_checksum) != checksum {
+                                    return Err(concat!(stringify!(#name), " checksums do not match"));
+                                }
+
+                                return Ok(#name {
+                                    #(
+                                        #outs6: #outs7?
+                                    ),*
+                                });
+                            },
+                            _ => {},
+                        }
+
+                        if input.len() <= field.length {
+                            return Err(concat!(stringify!(#name), " input ended before checksum"));
+                        }
+                        checksum += field.checksum;
+                        input = &input[field.length..];
+                    }
+                }
+            }
+        };
+    };
+    tokens
+}
+
+fn impl_fix_deserialize_enum(name: syn::Ident, variants: Vec<syn::Variant>) -> quote::Tokens {
+    let pairs: Vec<_> = variants.iter().map(|variant| {
+        match variant.data {
+            syn::VariantData::Tuple(ref fields) if fields.len() == 1 => {
+                let field = &fields[0];
+                match field.ty {
+                    syn::Ty::Path(ref o, ref p) => {
+                        let ty = syn::Ty::Path(o.clone(), p.clone());
+                        (variant.ident.clone(), ty)
+                    }
+                    _ => panic!("Only paths are supported."),
+                }
+            },
+            _ => panic!("Only one-field tuple variants are supported for enums."),
+        }
+    }).collect();
+
+    let names: Vec<_> = pairs.iter().map(|_| name.clone()).collect();
+    let cases: Vec<_> = pairs.iter().map(|p| p.0.clone()).collect();
+    let tys: Vec<_> = pairs.iter().map(|p| p.1.clone()).collect();
+    let dummy_const = syn::Ident::new(format!("_IMPL_FIX_DESERIALIZE_FOR_{}", name));
+
+    let tokens = quote! {
+        #[allow(non_upper_case_globals)]
+        const #dummy_const: () = {
+            extern crate fix;
+            use fix::detail::FixMessageType;
+
+            impl fix::detail::FixDeserializable for #name {
+                fn deserialize_from_fix(msg: fix::detail::FixMessage) -> Result<Self, fix::ParseError> {
+                    #(
+                        if msg.msg_type == #tys::MSG_TYPE {
+                            return Ok(#names::#cases(fix::detail::FixDeserializable::deserialize_from_fix(msg)?));
+                        }
+                    )*
+
+                    Err(concat!(stringify!(#name), " unknown FIX message"))
+                }
+            }
+        };
+    };
+    tokens
+}
+
+struct FixField {
+    id: u64,
+    ident: syn::Ident,
+//    ty: syn::Ty,
+}
+
+fn find_fix_fields(fields: &[syn::Field]) -> Vec<FixField> {
+    fields.iter().map(|field| {
+        let id = find_attr("id", &field.attrs);
+        let id: u64 = match id.parse() {
+            Ok(id) => id,
+            Err(e) => panic!("Could not parse ID as u64: {} {}", id, e),
+        };
+        FixField {
+            id,
+            ident: field.ident.clone().unwrap(),
+//            ty: field.ty.clone(),
+        }
+    }).collect()
+}
+
+fn find_attr(name: &str, attrs: &[syn::Attribute]) -> String {
+    let mut result = None;
+    for attr in attrs {
+        if let syn::AttrStyle::Outer = attr.style {
+            if let syn::MetaItem::NameValue(ident, lit) = attr.value.clone() {
+                if syn::Ident::new(name) == ident {
+                    if let syn::Lit::Str(value, _style) = lit {
+                        if result.is_none() {
+                            result = Some(value);
+                        } else {
+                            panic!("{} supplied twice", name);
+                        }
+                    } else {
+                        panic!("{} must be a string", name);
+                    }
+                }
+            }
+        }
+    }
+
+    match result {
+        Some(x) => x,
+        None => panic!("{} not found", name),
+    }
+}
+
+
+
+
+
